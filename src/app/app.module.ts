975879d07b2ad4e1a63ddc0e5ee01c45c3b50531
frank <kuete.frank1@gmail.com>
2019-02-02 01:02:27 +0100

    j'ai ajoute les boutons etun nouveau component app/menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
